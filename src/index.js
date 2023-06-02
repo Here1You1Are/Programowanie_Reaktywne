04f636f39491b0b6c836069c0b534c1bdf8b1d17
Here1You1Are <132080611+Here1You1Are@users.noreply.github.com>
2023-06-02 17:43:37 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,19 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Home from './components/home';
+import Posts from "./components/posts";
+import NotFound from "./components/notFound";
 import reportWebVitals from './reportWebVitals';
-import  'bootstrap/dist/css/bootstrap.min.css';
-
-import Counter from './components/counter';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Counter />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+
+                <Route path="/" element={<App/>}>
+
+                    <Route path="home" element={<Home/>}/>
+
+                    <Route path="posts" element={<Posts/>}/>
+
+                    <Route
+                        path="*"
+                        element={
+                            <NotFound/>
+                        }
+                    />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
